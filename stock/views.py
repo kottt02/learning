a3eab1ae61317e54533711765bccd6c6fc3eddd4
kottt02 <catblack2261@gmail.com>
2025-11-03 00:21:15 +0300

    Init project

--- stock/views.py
+++ stock/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+
 from stock.models import Stock
+
 
 def stock_list(request):
     stocks = Stock.objects.all()
-    return render(request, 'stocks.html', {'stocks': stocks})
+    context = {
+        'stocks': stocks,
+    }
+    return render(request, 'stocks.html', context)
